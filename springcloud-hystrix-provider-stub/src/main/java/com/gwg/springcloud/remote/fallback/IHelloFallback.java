14385d19ef9296cdfea87212137081896f827e57
admin <weigang.gao>
2019-03-14 21:11:29 +0800

    完善

---
+++ springcloud-hystrix-provider-stub/src/main/java/com/gwg/springcloud/remote/fallback/IHelloFallback.java
@@ -0,0 +1,33 @@
+package com.gwg.springcloud.remote.fallback;
+
+import com.gwg.springcloud.common.Result;
+import com.gwg.springcloud.remote.IHelloRemote;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 熔断处理
+ */
+@Component
+@Slf4j
+public class IHelloFallback implements FallbackFactory<IHelloRemote> {
+
+    public IHelloRemote create(Throwable throwable) {
+        final StringBuffer message = new StringBuffer("IHelloRemote fallback");
+        if(throwable != null && throwable instanceof HystrixTimeoutException){
+            message.append(" timeout ");
+            log.error(message.toString(), throwable);
+
+        }
+        return new IHelloRemote(){
+            private String msg = message.toString();
+
+            public Result<String> printServiceProvider(String name, int age) {
+                System.out.println("fallback ....");
+                return  Result.error(msg);
+            }
+        };
+    }
+}
